e05885c9ae5c373e711da792544ae0797a795534
Raj Parekh <rajparekh001@outlook.com>
2021-10-01 14:50:35 -0400

    find file

--- .github/actions/auto-generate-changelog/entrypoint.sh
+++ .github/actions/auto-generate-changelog/entrypoint.sh
@@ -3,6 +3,7 @@ set -e
 
 python -m pip install --upgrade pip setuptools wheel
 pwd
+find -name "requirements.txt"
 pip install -r rp-01/react-query-starwars/.github/actions/auto-generate-changelog/requirements.txt
 
 python /main.py
