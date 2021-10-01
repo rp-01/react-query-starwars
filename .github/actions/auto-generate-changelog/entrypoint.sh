ac66d9815273bc412d06226b0fb0204d694f0393
Raj Parekh <rajparekh001@outlook.com>
2021-10-01 14:27:53 -0400

    fix file path

--- .github/actions/auto-generate-changelog/entrypoint.sh
+++ .github/actions/auto-generate-changelog/entrypoint.sh
@@ -2,6 +2,6 @@
 set -e
 
 python -m pip install --upgrade pip setuptools wheel
-pip install -r requirements.txt
+pip install -r /.github/actions/auto-generate-changelog/requirements.txt
 
 python /main.py
