57280f325c5d18213498289ce8d425c36275d71b
Raj Parekh <rajparekh001@outlook.com>
2021-10-01 15:07:36 -0400

    print commiter

--- .github/actions/auto-generate-changelog/main.py
+++ .github/actions/auto-generate-changelog/main.py
@@ -134,6 +134,7 @@ class GithubChangelog:
         # References: https://pygithub.readthedocs.io/en/latest/github_objects/Repository.html#github.Repository.Repository
         g = github.Github(ACCESS_TOKEN)
         self.__repo = g.get_repo(REPO_NAME)
+        print(COMMITTER)
         self.__author = github.GithubObject.NotSet if COMMITTER == '' else github.InputGitAuthor(COMMITTER.split(' ')[0], COMMITTER.split(' ')[1])
 
     def get_data(self):
