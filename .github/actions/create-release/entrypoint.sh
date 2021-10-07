81fe9ef138c980573b8465f0fd386fdded194a6e
Raj Parekh <rajparekh001@outlook.com>
2021-10-07 14:49:02 -0400

    update file path in entrypoint

--- .github/actions/create-release/entrypoint.sh
+++ .github/actions/create-release/entrypoint.sh
@@ -2,5 +2,5 @@
 set -e
 
 python -m pip install --upgrade pip setuptools wheel
-pip install -r ./.github/actions/auto-generate-changelog/requirements.txt
-python ./.github/actions/auto-generate-changelog/main.py
+pip install -r ./.github/actions/create-release/requirements.txt
+python ./.github/actions/create-release/main.py
